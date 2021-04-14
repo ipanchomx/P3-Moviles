99c73033d65aec012df4e226c1af124e34a591b1
Edgar Medina <is715468@iteso.mx>
2021-04-14 13:10:33 -0500

    added share functionality

--- lib/home/noticias_ext_api/item_noticia.dart
+++ lib/home/noticias_ext_api/item_noticia.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_login/models/new.dart';
+import 'package:share/share.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ItemNoticia extends StatelessWidget {
   final New noticia;
@@ -12,7 +17,6 @@ class ItemNoticia extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.all(6.0),
         child: Card(
-          // margin: EdgeInsets.zero,
           child: IntrinsicHeight(
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -20,7 +24,8 @@ class ItemNoticia extends StatelessWidget {
                 Expanded(
                   flex: 1,
                   child: Image.network(
-                    "${noticia.urlToImage}",
+                    noticia.urlToImage ??
+                        "https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png",
                     fit: BoxFit.fitHeight,
                   ),
                 ),
@@ -65,6 +70,19 @@ class ItemNoticia extends StatelessWidget {
                             fontSize: 12,
                           ),
                         ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            GestureDetector(
+                                child: Icon(
+                                  Icons.share,
+                                  size: 20,
+                                ),
+                                onTap: () {
+                                  shareImage(noticia);
+                                }),
+                          ],
+                        )
                       ],
                     ),
                   ),
@@ -74,6 +92,24 @@ class ItemNoticia extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  void shareImage(New noticia) async {
+    String urlImage = noticia.urlToImage ??
+        "https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png";
+    final response = await get(Uri.parse(urlImage));
+    String extension = urlImage.split(".").last;
+    final bytes = response.bodyBytes;
+
+    final Directory temp = await getTemporaryDirectory();
+    final File imageFile = File('${temp.path}/tempImage');
+    imageFile.writeAsBytesSync(response.bodyBytes);
+
+    Share.shareFiles(
+      ['${temp.path}/tempImage.$extension'],
+      text:
+          'Checa está noticia ${noticia.title ?? "Noticia"} - ${noticia.description ?? "Sin descripcion"}" \n ${noticia.url ?? ""}',
     );
   }
 }
